1410d61e12b6b3572a70e5914ee3c9af87215884
Sagar Maurya <sagarmaurya010@gmail.com>
2026-01-31 16:33:00 +0530

    Added Java variables

---
+++ Basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Basic Variable Declaration
+        int age = 25;
+        System.out.println("Age: " + age);
+
+        // Multiple Variable Declaration
+        int x = 10, y = 20, z = 30;
+        System.out.println("X: " + x + ", Y: " + y + ", Z: " + z);
+
+        // Chained Assignment
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
+
+    }
+}
