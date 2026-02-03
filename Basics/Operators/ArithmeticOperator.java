e75902eac2b891d10d9266b9501e9d0674784486
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-03 20:53:27 +0530

    Added Operator: Arithmetic Operator

---
+++ Basics/Operators/ArithmeticOperator.java
@@ -0,0 +1,28 @@
+package Operators;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int value1 = 10;
+        int value2 = 3;
+
+        // Addition
+        int sum = value1 + value2;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int sub = value1 - value2;
+        System.out.println("Difference: " + sub);
+
+        // Multiplication
+        int mul = value1 * value2;
+        System.out.println("Multiplication: " + mul);
+
+        // Division
+        int div = value1 / value2;
+        System.out.println("Division: " + div);
+
+        // Modulus
+        int mod = value1 % value2;
+        System.out.println("Modulus: " + mod);
+    }
+}
