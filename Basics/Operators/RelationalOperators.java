9685a5a52e4c0b2fad0ec1fb3c6dc39945bd8964
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-04 07:26:00 +0530

    Added Logical Operators

--- Basics/Operators/RelationalOperators.java
+++ Basics/Operators/RelationalOperators.java
@@ -5,7 +5,7 @@ public class RelationalOperators {
         int a = 2;
         int b = 5;
 
-        System.out.println("a>b: " + (a>b));
+        System.out.println("a>b: " + ((a>b)));
         System.out.println("a<b: " + (a<b));
         System.out.println("a>=b: " + (a>=b));
         System.out.println("a<=b: " + (a<=b));
