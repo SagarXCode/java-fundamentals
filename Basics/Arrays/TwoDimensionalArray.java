a8f4f4f06c52b168c6bf6178201b25ccc2eb2266
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-06 21:48:45 +0530

    Added Two Dimensional Array

---
+++ Basics/Arrays/TwoDimensionalArray.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("Element at row 2, column 2: " + matrix[1][1]);
+    }
+}
