764adaf92fd69e6f3e690129786902770792a2dc
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-08 21:54:40 +0530

    Added Delete Element from Arrays

---
+++ Basics/Arrays/ArrayOperations/DeleteElement.java
@@ -0,0 +1,21 @@
+package Arrays.ArrayOperations;
+
+import java.util.Arrays;
+
+public class DeleteElement {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int[] newArr = new int[arr.length - 1];
+        int deleteIndex = 3;
+        
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if (i != deleteIndex) {
+                newArr[k] = arr[i];
+                k++;
+            }
+        }
+        System.out.println("Before deletion :" + Arrays.toString(arr));
+        System.out.println("Removing element at index 3...");
+        System.out.println("After deletion :" + Arrays.toString(newArr));
+    }
+}
