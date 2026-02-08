8eb350887a75b39213b6e3d0f67ed32a000d240b
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-08 22:04:15 +0530

    Updated Removing element from Array

--- Basics/Arrays/ArrayOperations/DeleteElement.java
+++ Basics/Arrays/ArrayOperations/DeleteElement.java
@@ -7,15 +7,15 @@ public class DeleteElement {
         int[] arr = new int[]{1, 2, 3, 4, 5};
         int[] newArr = new int[arr.length - 1];
         int deleteIndex = 3;
-        
+
         for (int i = 0, k = 0; i < arr.length; i++) {
             if (i != deleteIndex) {
                 newArr[k] = arr[i];
                 k++;
             }
         }
-        System.out.println("Before deletion :" + Arrays.toString(arr));
+        System.out.println("Before deletion: " + Arrays.toString(arr));
         System.out.println("Removing element at index 3...");
-        System.out.println("After deletion :" + Arrays.toString(newArr));
+        System.out.println("After deletion: " + Arrays.toString(newArr));
     }
 }
