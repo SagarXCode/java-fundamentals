2969aa264d8e2be678d045732d88f5d3ccf12545
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-12 21:31:04 +0530

    Add String length and charAt

---
+++ Basics/Strings/StringLengthAndCharAt.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StringLengthAndCharAt {
+    public static void main(String[] args) {
+        String str = "Programming";
+        // Prints length of string
+        System.out.println("Length of string: " + str.length());
+        // Prints character of given index
+        System.out.println("Character at index 3: " + str.charAt(3));
+    }
+}
