65d95f9df5972d98de466094d67ff22dd26879d5
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-14 20:40:32 +0530

    Add String Case Conversion

---
+++ Basics/Strings/StringCaseConversion.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringCaseConversion {
+    public static void main(String[] args) {
+        String str = "Development";
+
+        System.out.println("Lowercase: " + str.toLowerCase());
+        System.out.println("Uppercase: " + str.toUpperCase());
+    }
+}
