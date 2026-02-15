bc5884cb78fd6276c88db0e8e61aff23db94696a
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-15 20:27:45 +0530

    Add String Trim and Replace

---
+++ Basics/Strings/StringTrimAndReplace.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringTrimAndReplace {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        System.out.println(str.trim());
+        System.out.println(str.replace("Java", "Python"));
+    }
+}
