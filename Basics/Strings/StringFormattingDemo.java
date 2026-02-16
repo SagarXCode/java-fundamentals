2f38d5d2fe4b073e3a1f13505a7fd1b1fd8187de
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-16 06:31:53 +0530

    Add String formatting

---
+++ Basics/Strings/StringFormattingDemo.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringFormattingDemo {
+    public static void main(String[] args) {
+        String name = "Aditya";
+        int age = 22;
+
+        System.out.printf("Name: %s, Age: %d", name, age);
+    }
+}
