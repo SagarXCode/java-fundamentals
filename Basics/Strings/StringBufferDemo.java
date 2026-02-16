beafad912f821b0fdcfc66049b08c95ad6f2975e
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-16 06:20:59 +0530

    Add String Buffer demo

---
+++ Basics/Strings/StringBufferDemo.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Thread safe");
+        System.out.println(sb);
+    }
+}
