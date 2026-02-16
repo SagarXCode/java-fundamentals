85ee489d0591100e1200455a868005d5b7c0cb54
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-16 06:26:38 +0530

    Add Number to String conversion

---
+++ Basics/Strings/NumberToStringConversion.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class NumberToStringConversion {
+    public static void main(String[] args) {
+        int num = 100;
+        String str = String.valueOf(num);
+
+        System.out.println(str + 50);
+    }
+}
