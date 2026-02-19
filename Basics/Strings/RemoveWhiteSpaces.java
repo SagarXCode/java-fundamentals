c2cf5a344d0a1238d9d9e8c5078e2f190d470069
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-19 20:40:23 +0530

    Add Remove whitespace

---
+++ Basics/Strings/RemoveWhiteSpaces.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+        String str = "Java is awesome";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch != ' ') {
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
