6101683d0058b412c671dfe263e57f6c0862c353
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-20 20:59:08 +0530

    Add Class and Object

---
+++ Core Java/OOP/ClassAndObject.java
@@ -0,0 +1,22 @@
+package OOP;
+
+class Student {
+    int id;
+    String name;
+
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class ClassAndObject {
+    public static void main(String[] args) {
+        Student s1 = new Student();  // object creation
+
+        s1.id = 101;
+        s1.name = "Rahul";
+
+        s1.display();
+    }
+}
