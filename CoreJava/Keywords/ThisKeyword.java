e8a9f4d83ab8737089227e2a4f479e7b83847658
Sagar Maurya <sagarmaurya010@gmail.com>
2026-02-25 06:19:13 +0530

    Add This keyowrd

--- CoreJava/Keywords/ThisKeyword.java
+++ CoreJava/Keywords/ThisKeyword.java
@@ -1,5 +1,20 @@
 package Keywords;
 
-public class ThisKeyword {
+class Student {
+    int age;
 
+    Student(int age) {
+        this.age = age; // 'this' refers to the current instance of the class
+    }
+
+    void show() {
+        System.out.println("Age: " + age);
+    }
+}
+
+public class ThisKeyword {
+    public static void main(String[] args) {
+        Student s = new Student(21);
+        s.show();
+    }
 }
